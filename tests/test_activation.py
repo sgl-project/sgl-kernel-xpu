f4b4a32e0dc8303589b421cf8aa73901b360406f
Jin Hanyu <hanyu.jin@intel.com>
2025-07-31 18:56:11 -0700

    Add elementwise kernels.

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -4,12 +4,18 @@ import pytest
 import sgl_kernel
 import torch
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.xpu.is_available():
+    device = torch.device("xpu")
+else:
+    device = torch.device("cpu")
 
 @pytest.mark.parametrize("dim", [128, 256, 512, 2048, 4096, 11008, 16384])
 @pytest.mark.parametrize("batch_size", [1, 2, 4, 8, 16])
 @pytest.mark.parametrize("seq_len", [1, 2, 4, 8, 16, 32, 64, 128, 512])
 def test_fused_silu_mul(dim, batch_size, seq_len):
-    x = torch.randn(batch_size, seq_len, 2 * dim).to(0).to(torch.float16)
+    x = torch.randn(batch_size, seq_len, 2 * dim).to(device).to(torch.float16)
     y_ref = x[..., dim:] * torch.nn.functional.silu(x[..., :dim])
     y = sgl_kernel.silu_and_mul(x)
     torch.testing.assert_close(y_ref, y, rtol=1e-3, atol=1e-3)
@@ -19,7 +25,7 @@ def test_fused_silu_mul(dim, batch_size, seq_len):
 @pytest.mark.parametrize("batch_size", [1, 2, 4, 8, 16])
 @pytest.mark.parametrize("seq_len", [1, 2, 4, 8, 16, 32, 64, 128, 512])
 def test_fused_gelu_tanh_mul(dim, batch_size, seq_len):
-    x = torch.randn(batch_size, seq_len, 2 * dim).to(0).to(torch.float16)
+    x = torch.randn(batch_size, seq_len, 2 * dim).to(device).to(torch.float16)
     y_ref = x[..., dim:] * torch.nn.functional.gelu(x[..., :dim], approximate="tanh")
     y = sgl_kernel.gelu_tanh_and_mul(x)
     torch.testing.assert_close(y_ref, y, rtol=1e-3, atol=1e-3)
@@ -29,7 +35,7 @@ def test_fused_gelu_tanh_mul(dim, batch_size, seq_len):
 @pytest.mark.parametrize("batch_size", [1, 2, 4, 8, 16])
 @pytest.mark.parametrize("seq_len", [1, 2, 4, 8, 16, 32, 64, 128, 512])
 def test_fused_gelu_mul(dim, batch_size, seq_len):
-    x = torch.randn(batch_size, seq_len, 2 * dim).to(0).to(torch.float16)
+    x = torch.randn(batch_size, seq_len, 2 * dim).to(device).to(torch.float16)
     y_ref = x[..., dim:] * torch.nn.functional.gelu(x[..., :dim], approximate="none")
     y = sgl_kernel.gelu_and_mul(x)
     torch.testing.assert_close(y_ref, y, rtol=1e-3, atol=1e-3)

610f8fb007d66af3cf80694ada211bb9614efdba
jiwei1.sun <jiwei1.sun@intel.com>
2025-10-23 11:00:19 +0800

    fix lint

--- src/sycl/comm/common.h
+++ src/sycl/comm/common.h
@@ -14,15 +14,14 @@ namespace {
     }                                            \
   }()
 
-
 // dispatch bool
 #define AT_DISPATCH_BOOL_NO_RETURN(BOOL_V, BOOL_NAME, ...) \
-  if (BOOL_V) {                                \
-    constexpr bool BOOL_NAME = true;           \
-    __VA_ARGS__;                               \
-  } else {                                     \
-    constexpr bool BOOL_NAME = false;          \
-    __VA_ARGS__;                               \
-  }                                            \
+  if (BOOL_V) {                                            \
+    constexpr bool BOOL_NAME = true;                       \
+    __VA_ARGS__;                                           \
+  } else {                                                 \
+    constexpr bool BOOL_NAME = false;                      \
+    __VA_ARGS__;                                           \
+  }
 
-} // namespace
+}  // namespace

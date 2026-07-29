ca06cbfbb3f3e2f33c87c3de629fd1872b7e64ae
jiayisunx <jiayi.sun@intel.com>
2026-07-29 23:54:24 +0800

    Add SYCL support for flash_compress128_prefill

--- src/torch_extension_sycl.cc
+++ src/torch_extension_sycl.cc
@@ -356,6 +356,11 @@ TORCH_LIBRARY_FRAGMENT(sgl_kernel, m) {
       "flash_compress128_decode(Tensor! kv_buffer, Tensor kv_input, Tensor! kv_output, Tensor ape, Tensor plan_d) "
       "-> ()");
   m.impl("flash_compress128_decode", torch::kXPU, &at::native::xpu::flash_compress128_decode);
+
+    m.def(
+            "flash_compress128_prefill(Tensor! kv_buffer, Tensor kv_input, Tensor! kv_output, Tensor ape, Tensor plan_c, "
+            "Tensor plan_w) -> ()");
+    m.impl("flash_compress128_prefill", torch::kXPU, &at::native::xpu::flash_compress128_prefill);
 }
 
 REGISTER_EXTENSION(common_ops)
